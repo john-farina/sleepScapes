607ae3b2305336ca5ae6fcca6f861e03b7184c32
john-farina <clichecontent@gmail.com>
2022-11-23 20:16:56 -0500

    import map brokken i need to replace this branch

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,7 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
-<<<<<<< HEAD
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-=======
->>>>>>> 36171bf0f4b6a7c2de91e4ed6a2c9ff777562db6
 pin "howler", to: "https://ga.jspm.io/npm:howler@2.2.3/dist/howler.js"
