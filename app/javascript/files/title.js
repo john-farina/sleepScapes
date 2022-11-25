1014b752a21fa02233346731a5977548285d71ea
john-farina <clichecontent@gmail.com>
2022-11-25 16:06:41 -0500

    animation tweaks

--- app/javascript/files/title.js
+++ app/javascript/files/title.js
@@ -1,7 +1,23 @@
 const animationCont = document.querySelector("#animationContainer");
-
+let animationGoing = false;
 if (animationCont) {
   setTimeout(() => {
     animationCont.classList.add("scrollAnimation");
+    animationGoing = true;
+    setTimeout(() => {
+      animationCont.classList.remove("scrollAnimation");
+      animationGoing = false;
+    }, 1700);
   }, 300);
+
+  animationCont.addEventListener("click", () => {
+    if (!animationGoing) {
+      animationCont.classList.add("scrollAnimation");
+      animationGoing = true;
+      setTimeout(() => {
+        animationCont.classList.remove("scrollAnimation");
+        animationGoing = false;
+      }, 1700);
+    }
+  });
 }
