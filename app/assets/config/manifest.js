c4352d777e936db93e2ee987ba6334eda25c3ffa
john-farina <clichecontent@gmail.com>
2022-11-25 20:58:49 -0500

    change?

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
-//= link_tree ../videos .mp4
+//= link_tree ../videos
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
