eec57427a6e987260d9fc799c9263ce240c2cc25
john-farina <clichecontent@gmail.com>
2022-12-03 22:45:00 -0500

    added better sign in, and titles for categry pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,15 @@ module ApplicationHelper
 
   def title_helper(title)
     if title.present?
+      if title == "sleepDefaultTitleWierdName_-_-"
+        "[sleepScapes]"
+      elsif title == "studyDefaultTitleWierdName_-_-"
+        "[studyScapes]"
+      elsif title == "distractDefaultTitleWierdName_-_-"
+      "[distractionScapes]"
+      else
       title + "[soundScapes]"
+      end
     else
       "[soundScapes]"
     end
--- app/javascript/files/headerForms.js
+++ app/javascript/files/headerForms.js
@@ -1,5 +1,6 @@
 const signInForm = document.querySelector("#signInHeader");
 const signInBtn = document.querySelector("#signInButton");
+const pageHeader = document.querySelector("#pageHeader");
 let signInOpen = false;
 if (signInBtn) {
   signInForm.style.display = "none";
@@ -8,9 +9,11 @@ if (signInBtn) {
     if (!signInOpen) {
       signInOpen = true;
       signInForm.style.display = "flex";
+      pageHeader.style.width = "350px";
     } else {
       signInOpen = false;
       signInForm.style.display = "none";
+      pageHeader.style.width = "";
     }
   });
 }
