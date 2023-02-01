90b602af41919191a15ec5adabb4b6221b682853
john-farina <clichecontent@gmail.com>
2023-02-01 12:53:07 -0500

    trying to fix blocked host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  config.hosts << "soundscapes.up.railway.app"
+  # config.hosts << "soundscapes.up.railway.app"
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
